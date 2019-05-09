999e1899034b164ca268d496fd947052a0d8b75d
rickybell <henrique.belloube@gmail.com>
2019-05-09 01:24:28 -0300

    the initial structure of the project was created with Node + Express + React

---
+++ api/routes/testAPI.js
@@ -0,0 +1,7 @@
+// import express from 'express'
+var express = require("express");
+var router = express.Router();
+router.get("/", function(req, res, next) {
+    res.send("API is working properly");
+});
+module.exports = router;
